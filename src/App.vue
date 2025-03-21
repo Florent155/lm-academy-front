f6878721346cc996ff21f3b2e78b1260a20584f0
Florent155 <florentplakolli2@gmail.com>
2025-03-21 19:29:19 +0100

    Initial Commit for Vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!--ketu shenohet kodi i html-->
+
+  <p>{{ message }}</p>
+  <p class="text-green-200">
+Florent
+  </p>
+</template>
+
+
+<script setup>
+import {ref} from "vue";
+const message = ref("Florent");
+
+setTimeout(() => {
+  message.value = "hello!"
+}, 5000);
+</script>
+ 
+<style>
+
+p{
+  color:red;
+}
+
+</style>
